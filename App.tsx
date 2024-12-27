31bd0b9874b3fae364c6c50a5b57ebec46a4c2ba
Đặng Ngọc Tài <2001224231@hufi.edu.vn>
2024-12-27 18:43:07 +0700

    video29

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState<string>("Dang Ngoc Tai");
+  const [age, setAge] = useState<number>(20);
+
+  const [person, setPerson] = useState<{ name: string, age: number }>({ name: "Dang Ngoc Tai", age: 20 });
+
   return (
     <View style={styles.container}>
+      <View>
+        {/* <Text style={styles.test}>{name}</Text>
+        <Text style={styles.test}>{age}</Text> */}
+        <Text style={styles.test}>{person.name}</Text>
+        <Text style={styles.test}>{person.age}</Text>
+
+      </View>
       <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -14,7 +28,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    paddingTop: 10,
+    paddingHorizontal: 20,
+  },
+  test: {
+    color: 'red',
   },
 });
